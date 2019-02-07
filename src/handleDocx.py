4322192e3605c80b7aa97221309fb151ec0ca099
Thomas Lawless <tlawless94@gmail.com>
2019-02-07 13:10:48 -0600

    writing to an output file

--- src/handleDocx.py
+++ src/handleDocx.py
@@ -1,8 +1,25 @@
 import os
-from docx import Document
+import docx
 
 baseDir = os.path.abspath(os.path.join(
-    os.path.dirname(__file__), '..', 'templates'))
+    os.path.dirname(__file__), '..', 'writing'))
 
-for root, subdirs, files in os.walk(baseDir):
-    print('--\\nroot = ' + root)
+targetDir = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), '..', 'outputTxtFile'))
+
+print('basedir', baseDir)
+
+
+def traverseFiles(folder):
+    print('running')
+    outputFile = docx.Document()
+    for root, subdirs, files in os.walk(folder):
+        for curFile in files:
+            if curFile.endswith('.docx'):
+                curDoc = docx.Document(root + '/' + curFile)
+                for paragraph in curDoc.paragraphs:
+                    outputFile.add_paragraph(paragraph.text)
+    outputFile.save(targetDir + '/data.txt')
+
+
+traverseFiles(baseDir)
